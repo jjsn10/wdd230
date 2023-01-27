9233c4e13fca53e06753b954697a2bc67e087efd
jjsn10 <san14038@byui.edu>
2023-01-26 19:31:49 -0700

    update Assignment Portal I

--- scripts/datetime.js
+++ scripts/datetime.js
@@ -12,11 +12,11 @@ const getCurrentDate = new Date();
 //getting and format the year as a number
 const year = getCurrentDate.getFullYear();
 
-let html_year=document.querySelector('.year');
+let html_year=document.querySelector('#year');
 html_year.textContent = year;
 
 let date_formated = getCurrentDate.toLocaleDateString('en-US',options_date)
 let current_time = getCurrentDate.toLocaleTimeString('en-US',options_time)
 
-let html_date_time=document.querySelector('.date_time');
+let html_date_time=document.querySelector('#date_time');
 html_date_time.textContent =` ${date_formated} ${current_time}`
