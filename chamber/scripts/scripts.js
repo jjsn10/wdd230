06851b4d5319e5e29543a3df6b65aa386fbbb227
jjsn10 <san14038@byui.edu>
2023-01-29 23:28:18 -0700

    update chamber

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    console.log("It Worked!")
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburguerBtn").classList.toggle("open");
+}
+const x = document.getElementById("hamburguerBtn");
+
+x.onclick = toggleMenu;
