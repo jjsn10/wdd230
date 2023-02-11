052b9c066afbc2ca41be50bc0cee96989345d442
jjsn10 <sanche37@ensign.edu>
2023-02-10 17:18:25 -0700

    upload greeting for Monday and Tuesdays

--- chamber/scripts/datetime.js
+++ chamber/scripts/datetime.js
@@ -1,3 +1,15 @@
+//Creating Array of Months
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+//Const for Short weekday
+const optionShort ={
+    weekday:"short"
+};
+//Const for long weekday
+const optionLong = {
+    weekday:"long"
+};
+
 const options_date = {
     day:'2-digit',
     month:'2-digit',
@@ -7,10 +19,13 @@ const options_time ={
     hour12:false
 }
 //Getting the date without format
+//const getCurrentDate = new Date('02-08-2023');
 const getCurrentDate = new Date();
 
 //getting and format the year as a number
 const year = getCurrentDate.getFullYear();
+const day = getCurrentDate.getDay();
+console.log("27:",day);
 
 let html_year=document.querySelector('#year');
 html_year.textContent = year;
@@ -20,4 +35,22 @@ let current_time = getCurrentDate.toLocaleTimeString('en-US',options_time)
 
 let html_date_time=document.querySelector('#date_time');
 html_date_time.textContent =` ${date_formated} ${current_time}`
+
+//Getting Current short Day a long Date
+const sDay= getCurrentDate.toLocaleDateString('en-US',optionShort);
+const lDay= getCurrentDate.toLocaleDateString('en-US', optionLong);
+const numberDay = getCurrentDate.getDate();
+const monthName = month[getCurrentDate.getMonth()];
+
+let dateinf = document.getElementById("date");
+console.log("45",sDay);
+console.log("46",lDay);
+if (sDay === "Mon" || sDay === "Tue"){
+    dateinf.textContent = "Come join us for the chamber meet and greet Wednesday at 7:00 p.m";
+    //console.log(dateinf);
+}else{
+    dateinf.textContent =`${lDay}, ${numberDay} ${monthName} ${year}`;
+    //console.log(dateinf.value);
+    //console.log(`${lDay}, ${numberDay} ${monthName} ${year} `);
+}
 
