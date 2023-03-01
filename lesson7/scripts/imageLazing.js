683f09e0957c36e708ed386b789c71cb1972a986
jjsn10 <sanche37@ensign.edu>
2023-02-28 17:46:50 -0700

    Progressive Loading of Images

---
+++ lesson7/scripts/imageLazing.js
@@ -0,0 +1,35 @@
+//getting all images with data-src 
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+console.log(imagesToLoad);
+
+const imgOptions = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 1
+};
+
+const loadImages = (image) =>{
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () =>{
+        image.removeAttribute("data-src");
+    };
+};
+
+if ('IntersectionObserver' in window){
+    const observer = new IntersectionObserver((items, observer)=>{
+        items.forEach((item)=>{
+            if (item.isIntersecting){
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+    
+    imagesToLoad.forEach((img) =>{
+        observer.observe(img);
+    });
+}else{
+    imagesToLoad.forEach((img)=>{
+        loadImages(img);
+    });
+
+}
