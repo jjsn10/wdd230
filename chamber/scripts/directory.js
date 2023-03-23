9571d58388605cfda1066f4c7c32fda7abd1ad96
jjsn10 <san14038@byui.edu>
2023-03-23 08:16:37 -0600

    update directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,45 +4,71 @@ async function getBusinessData(){
     const response = await fetch(url);
     const data = await response.json();
     console.log(data.business);
-    //displayProphets(data.prophets);
+    displayBusinesses(data.business);
 }
 
 getBusinessData();
 
 const displayBusinesses = (businesses) =>{
-    const cards = document.querySelector('div.cards');
+    const article = document.querySelector('article.grid');
 
     businesses.forEach((business)=>{
         //create elements to add to the div.cards elements
         let section = document.createElement('section');
         let h3 = document.createElement('h3');
-        let p = document.createElement('p');
+        let name = document.createElement('p');
+        let phone =document.createElement('p');
         /*let birth_date = document.createElement('span');
         let birth_place = document.createElement('span');*/
         let logo = document.createElement('img');
-        let a = document.createElement('a');
+        let website = document.createElement('a');
 
         //Build the h2 content out to show the prophet's full name - finish the template string
-        h2.textContent =`${business.name} ${business.lastname}`;
+        h3.textContent =`${business.name}`;
+
+        name.textContent = `${business.address}`;
+        
+        phone.textContent = `${business.phone_number}`;
+
+        website.textContent = `${business.website}`;
+        website.setAttribute('href',business.website);
+        website.setAttribute('target','_blank');
 
         /*birth_date.textContent = `Date of Birth: ${business.birthdate}`
 
         birth_place.textContent = `Place of Birth: ${business.birthplace}`*/
         
         //Build the image portrait by setting all the relevant attribute
-        logo.setAttribute('src',business.imageurl);
-        logo.setAttribute('alt',`Portrait of ${business.name}_${business.lastname}`);
+        logo.setAttribute('src',business.image);
+        logo.setAttribute('alt',`Portrait of ${business.name}`);
         logo.setAttribute('loading', 'lazy');
-        logo.setAttribute('width','340');
-        logo.setAttribute('heigth', '440');
+        /*logo.setAttribute('width','340');
+        logo.setAttribute('heigth', '100');*/
 
         //Append the section(card) with the created elements
-        section.appendChild(h2);
-        section.appendChild(birth_date);
-        section.appendChild(birth_place);
-        section.appendChild(portrait);
+        section.appendChild(logo);
+        section.appendChild(h3);
+        section.appendChild(phone);
+        section.appendChild(name);
+        section.appendChild(website);
+        /*section.appendChild(birth_place);*/
+        
 
-        cards.appendChild(card);
+        article.appendChild(section);
         
     })
 }
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click",()=>{
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", ()=>{
+    display.classList.add("list");
+    display.classList.remove("grid");
+});
